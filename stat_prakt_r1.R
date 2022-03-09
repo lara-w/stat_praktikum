50c3961f1ea78dc26d60562b25b6e4606021143c
lara-w <64131248+lara-w@users.noreply.github.com>
2022-03-09 14:24:47 +0100

    Add files via upload

---
+++ stat_prakt_r1.R
@@ -0,0 +1,5 @@
+
+FIRE_3_clin_data <- read_excel("Desktop/Statistisches Praktikum/FIRE-3_clin_data.xlsx")
+FIRE_3_EMT_genes_final <- read_excel("Desktop/Statistisches Praktikum/FIRE-3_EMT_genes_final.xlsx")
+View(FIRE_3_clin_data)  
+View(FIRE_3_EMT_genes_final)         
