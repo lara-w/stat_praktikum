e59ec2df1561d413b905a49084b2ef457c089c43
lara-w <64131248+lara-w@users.noreply.github.com>
2022-03-09 16:22:12 +0100

    Update stat_prakt_r1.R

--- stat_prakt_r1.R
+++ stat_prakt_r1.R
@@ -1,12 +1,14 @@
-
+### Einlesen Dateien
 FIRE_3_clin_data <- read_excel("Desktop/Statistisches Praktikum/FIRE-3_clin_data.xlsx")
 FIRE_3_EMT_genes_final <- read_excel("Desktop/Statistisches Praktikum/FIRE-3_EMT_genes_final.xlsx")
 
+### Umbenennen von CRF in pat_nr fuer merge
 FIRE_3_EMT_genes_final$pat_nr <- FIRE_3_EMT_genes_final$CRF   
 
 str(FIRE_3_clin_data)
 str(FIRE_3_EMT_genes_final)
 
+### merge by pat_nr, NA aussortiert
 fire3_complete <- merge(FIRE_3_clin_data, FIRE_3_EMT_genes_final, by = "pat_nr")
 
 str(FIRE_3_EMT_genes_final)
